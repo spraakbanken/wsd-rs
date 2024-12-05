ec115a1993c5e1cb01ab0d06f69dfa316a5cc4c6
Kristoffer Andersson <kristoffer.andersson@gu.se>
2024-12-05 14:46:11 +0100

    fix: handle Javaesque cli options

--- crates/saldowsd/src/main.rs
+++ crates/saldowsd/src/main.rs
@@ -1,49 +1,30 @@
-use clap::{error::ErrorKind, CommandFactory, Parser};
-use env_logger::Env;
 use log::LevelFilter;
-use options::{Args, Format};
+use options::Args;
 use saldo::SaldoLexicon;
 
 mod options;
 
-fn main() {
-    let args = Args::parse();
-    dbg!(&args);
+fn main() -> miette::Result<()> {
+    let argv: Vec<String> = std::env::args().collect();
+    let args = Args::parse(&argv).map_err(|err| {
+        usage();
+        err
+    })?;
 
     configure_logging(args.verbose);
-    let eval = matches!(args.format, Format::Eval);
-    if eval && args.eval_lemmas.is_none() {
-        let mut cmd = Args::command();
-        cmd.error(
-            ErrorKind::MissingRequiredArgument,
-            "evalLemmas not specified, required when --format=eval",
-        )
-        .exit();
-    }
-    if eval && args.eval_key.is_none() {
-        let mut cmd = Args::command();
-        cmd.error(
-            ErrorKind::MissingRequiredArgument,
-            "evalKey not specified, required when --format=eval",
-        )
-        .exit();
-    }
-    if eval && args.for_lemma.is_none() {
-        let mut cmd = Args::command();
-        cmd.error(
-            ErrorKind::MissingRequiredArgument,
-            "forLemma not specified, required when --format=eval",
-        )
-        .exit();
-    }
 
-    let saldo = args
-        .saldo
-        .as_ref()
-        .map(|saldo_file| SaldoLexicon::new(saldo_file));
     // let wsd = WSDApplication::make(saldo, args.app_name);
+    let saldo = match &args.saldo {
+        None => None,
+        Some(saldo_file) => Some(SaldoLexicon::new(saldo_file)?),
+    };
+    Ok(())
 }
 
+fn usage() {
+    eprintln!("Usage: saldowsd -appName=APP_NAME [-saldo=SALDO]");
+    eprintln!("");
+}
 fn configure_logging(level: u8) {
     let log_level = match level {
         3 => LevelFilter::Trace,
