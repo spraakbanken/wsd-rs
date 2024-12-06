bef324f056592dd0eb6711cb96d9ff578945f649
Kristoffer Andersson <kristoffer.andersson@gu.se>
2024-12-06 13:38:45 +0100

    feat: use wsd-application in saldowsd

--- crates/saldowsd/src/main.rs
+++ crates/saldowsd/src/main.rs
@@ -1,6 +1,13 @@
+use std::io;
+
 use log::LevelFilter;
+use miette::IntoDiagnostic;
 use options::Args;
 use saldo::SaldoLexicon;
+use wsd_application::{
+    make_wsd_application,
+    wsd_application::{evaluate, read_sentences},
+};
 
 mod options;
 
@@ -13,11 +20,72 @@ fn main() -> miette::Result<()> {
 
     configure_logging(args.verbose);
 
-    // let wsd = WSDApplication::make(saldo, args.app_name);
     let saldo = match &args.saldo {
         None => None,
         Some(saldo_file) => Some(SaldoLexicon::new(saldo_file)?),
     };
+
+    let wsd = make_wsd_application(saldo.as_ref(), &args.app_name, &argv)?;
+
+    if args.eval {
+        evaluate(wsd, &args.eval_lemmas.unwrap(), &args.eval_key.unwrap());
+        return Ok(());
+    }
+
+    // let mut ratios = None;
+    if !args.for_lemma.is_none() {
+        todo!("ratios = Some(HashMap::new())");
+    }
+    let mut total_sentences = 0;
+    let mut next_print = 100000;
+
+    let mut stdin = io::stdin().lock();
+
+    loop {
+        let text = read_sentences(
+            &mut stdin,
+            saldo.as_ref(),
+            args.sbxml,
+            args.batch_size,
+            args.split_mwes,
+            args.split_compounds,
+        )
+        .into_diagnostic()?;
+        if text.len() == 0 {
+            break;
+        }
+        total_sentences += text.len();
+        if total_sentences > next_print {
+            log::info!("{}", next_print);
+            next_print += 100000;
+        }
+
+        if !args.for_lemma.is_none() {
+            todo!("forLemma not supported yet");
+        } else {
+            let result = wsd.disambiguate_text(text);
+            for p in result {
+                for i in 0..p.0.len() {
+                    print!("{}\t", &p.0[i]);
+                    match &p.1[i] {
+                        None => println!("_"),
+                        Some(scores) => println!("{}", join_to_string(scores)),
+                    }
+                }
+                println!();
+            }
+
+            if total_sentences >= args.max_sen {
+                break;
+            }
+        }
+    }
+
+    if !args.for_lemma.is_none() {
+        todo!("printRatios(ratios)");
+    }
+
+    // TODO split into chunks and use thread pool
     Ok(())
 }
 
@@ -25,12 +93,25 @@ fn usage() {
     eprintln!("Usage: saldowsd -appName=APP_NAME [-saldo=SALDO]");
     eprintln!("");
 }
+
+fn join_to_string(vs: &[f32]) -> String {
+    if vs.is_empty() {
+        return String::new();
+    }
+    let mut out = String::with_capacity(vs.len() * 2);
+    out.push_str(&vs[0].to_string());
+    for v in &vs[1..] {
+        out.push_str(&format!("|{}", v));
+    }
+    out
+}
 fn configure_logging(level: u8) {
     let log_level = match level {
-        3 => LevelFilter::Trace,
-        1 => LevelFilter::Info,
-        2 => LevelFilter::Debug,
-        _ => LevelFilter::Debug,
+        0 => LevelFilter::Error,
+        1 => LevelFilter::Warn,
+        2 => LevelFilter::Info,
+        3 => LevelFilter::Debug,
+        _ => LevelFilter::Trace,
     };
     env_logger::builder().filter_level(log_level).init();
 }
--- crates/saldowsd/src/options.rs
+++ crates/saldowsd/src/options.rs
@@ -1,52 +1,119 @@
-use std::path::PathBuf;
+use wsd_application::UsageError;
 
-#[derive(Debug, clap::Parser)]
-#[clap(author,version,about,long_about=None)]
+#[derive(Debug)]
 pub struct Args {
     /// load saldo from this file
-    #[clap(long)]
-    pub saldo: Option<PathBuf>,
+    pub saldo: Option<String>,
     /// app-name to use
-    #[clap(long = "appName")]
     pub app_name: String,
     /// Format of the output
-    #[clap(long, value_enum, default_value = "sbxml")]
-    pub format: Format,
+    pub sbxml: bool,
+    pub eval: bool,
     /// Should MWEs be split?
-    #[clap(long = "splitMWEs", value_enum, default_value = "false")]
-    pub split_mwes: Bool,
+    pub split_mwes: bool,
     /// Should Compunds be split?
-    #[clap(long = "splitCompounds", value_enum, default_value = "true")]
-    pub split_compounds: Bool,
+    pub split_compounds: bool,
     /// The size of each batch
-    #[clap(long = "batchSize", default_value = "1")]
-    pub batch_size: u64,
+    pub batch_size: usize,
     /// evalLemmas
-    #[clap(long = "evalLemmas")]
     pub eval_lemmas: Option<String>,
     /// evalKey
-    #[clap(long = "evalKey")]
     pub eval_key: Option<String>,
     /// forLemma
-    #[clap(long = "forLemma")]
     pub for_lemma: Option<String>,
     /// The maximum sense
-    #[clap(long = "maxSen", default_value = "2147483647")]
     pub max_sen: usize,
     /// Verbosity
-    #[clap(long,short='v',action=clap::ArgAction::Count)]
     pub verbose: u8,
 }
 
-#[derive(Debug, Clone, Copy, clap::ValueEnum)]
-pub enum Format {
-    Tab,
-    Sbxml,
-    Eval,
-}
+impl Args {
+    pub fn parse(argv: &[String]) -> Result<Self, UsageError> {
+        let mut saldo = None;
+        let mut app_name_opt = None;
+        let mut sbxml = true;
+        let mut eval = false;
+        let mut split_mwes = false;
+        let mut split_compounds = true;
+        let mut batch_size = 1;
+        let mut eval_lemmas = None;
+        let mut eval_key = None;
+        let mut max_sen = u32::MAX as usize;
+        let mut for_lemma = None;
+        let mut verbose = 0;
 
-#[derive(Debug, Clone, Copy, clap::ValueEnum)]
-pub enum Bool {
-    True,
-    False,
+        for a in argv {
+            if let Some(saldo_file) = a.strip_prefix("-saldo=") {
+                saldo = Some(saldo_file.to_string());
+            } else if let Some(app_name) = a.strip_prefix("-appName=") {
+                app_name_opt = Some(app_name.to_string());
+            } else if a == "-format=tab" {
+                sbxml = false;
+            } else if a == "-format=sbxml" {
+                sbxml = true;
+            } else if a == "-format=eval" {
+                eval = true;
+            } else if let Some(val) = a.strip_prefix("-splitMWEs=") {
+                split_mwes = val.parse().map_err(|_| UsageError::BadValue {
+                    param: "-splitMWEs".into(),
+                    value: val.into(),
+                })?;
+            } else if let Some(val) = a.strip_prefix("-splitCompounds=") {
+                split_compounds = val.parse().map_err(|_| UsageError::BadValue {
+                    param: "-splitCompounds".into(),
+                    value: val.into(),
+                })?;
+            } else if let Some(val) = a.strip_prefix("-batchSize=") {
+                batch_size = val.parse().map_err(|_| UsageError::BadValue {
+                    param: "-batchSize".into(),
+                    value: val.into(),
+                })?;
+            } else if let Some(val) = a.strip_prefix("-maxSen=") {
+                max_sen = val.parse().map_err(|_| UsageError::BadValue {
+                    param: "-maxSen".into(),
+                    value: val.into(),
+                })?;
+            } else if let Some(val) = a.strip_prefix("-evalLemmas=") {
+                eval_lemmas = Some(val.to_string());
+            } else if let Some(val) = a.strip_prefix("-evalKey=") {
+                eval_key = Some(val.to_string());
+            } else if let Some(val) = a.strip_prefix("-forLemma=") {
+                for_lemma = Some(val.to_string());
+            } else if a == "-verbose" || a == "-v" {
+                verbose += 1;
+            } else if a == "-verbose=true" {
+                verbose = 2;
+            }
+        }
+        if eval && eval_lemmas.is_none() {
+            return Err(UsageError::missing_required_argument(
+                "-evalLemmas not specified, required when --format=eval",
+            ));
+        }
+        if eval && eval_key.is_none() {
+            return Err(UsageError::missing_required_argument(
+                "-evalKey not specified, required when --format=eval",
+            ));
+        }
+        if eval && for_lemma.is_none() {
+            return Err(UsageError::missing_required_argument(
+                "-forLemma not specified, required when --format=eval",
+            ));
+        }
+        Ok(Self {
+            saldo,
+            app_name: app_name_opt
+                .ok_or_else(|| UsageError::missing_required_argument("-appName not specified"))?,
+            sbxml,
+            eval,
+            split_mwes,
+            split_compounds,
+            batch_size,
+            eval_lemmas,
+            eval_key,
+            for_lemma,
+            max_sen,
+            verbose,
+        })
+    }
 }
